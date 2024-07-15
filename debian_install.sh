4c1f9f67e0ca821d5fe1022afccd4471210b1cf3
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2024-07-15 19:48:00 -0300

    Add files via upload

--- debian_install.sh
+++ debian_install.sh
@@ -1,44 +1,67 @@
-#!/bin/bash
-#Instalador de debiaCopiar archivos de configuracin
+#!/bin/sh
+# Creador: TakYzGG
+
+# Variables
+bashrc=https://raw.githubusercontent.com/TakYzGG/Void-Linux/main/Utilidades/.bashrc
+fluxbox=https://github.com/TakYzGG/Dotfiles
 
 # Actualizar el sistema
-echo "Actualizando el sistema"
+echo "Actualizando el sistema..."
 apt install update
 
 # Instalar servidor grafico y terminal
-echo "Instalando el servidor grafico"
+echo "Instalando el servidor grafico..."
 apt install -y xorg xinit lxterminal
 
+# Instalar compresores
+apt install -y zip unzip p7zip
+
+# Añadir compatibilidad con exfat
+echo "Añadiendo compatibilidad con exfat..."
+apt install -y exfat-fuse
+
+# Instalar Fluxbox y complementos
+echo "Instalando Fluxbox..."
+apt install -y fluxbox dmenu feh
+
+# Instalar Programas adicionales
+echo "Instalando programas adicionales..."
+apt install -y wget git firefox btop galculator xreader mirage arandr l3afpad gparted xarchiver thunar thunar-volman thunar-archive-plugin pavucontrol vlc audacious libreffice lxappearance
+
+# Configurar Fluxbox
+echo "Configurando fluxbox..."
+read -p "¿Cual es tu nombre de usuario?: " usuario
+git clone $fluxbox
+rm -rf /home/$usuario/.fluxbox
+mv Dotfiles/.fluxbox /home/$usuario
+mv Dotfiles/Wallpapers /home/$usuario
+rm -rf Dotfiles
+
+# Configurar bash
+echo "Configurando bash..."
+rm -rf /home/$usuario/.bashrc
+rm -rf /root/.bashrc
+wget $bashrc
+cp .bashrc /home/$usuario
+cp .bashrc /root
+
 # Instalar pulseaudio y habilitarlo
-echo "Instalando pulseaudio"
+echo "Instalando pulseaudio..."
 apt install -y pulseaudio pavucontrol
 sistemctl --user enable pulseaudio
 sistemctl --user start pulseaudio
 
 # Instalar NetworkManager y habilitarlo
-echo "Instalando Network Manager"
-apt install -y network-manager network-manager-gnome
+echo "Instalando Network Manager..."
+apt install -y network-manager
 sistemctl --user enable NetworkManager
 sistemctl --user start NetworkManager
 
-# Instalar Programas adicionales
-echo "Instalando programas adicionales"
-apt install -y libreoffice
-apt install -y firefox-esr
-apt install -y leafpad
-apt install -y vlc
-apt install -y gparted
-apt install -y thunar
-apt install -y galculator
-apt install -y dmenu
-apt install -y xreader
-apt install -y audacius
-apt install -y xarchiver
-
-# Instalar Fluxbox y complementos
-echo "Instalando Fluxbox"
-apt install -y fluxbox feh
+# Instalar lxdm y habilitarlo
+echo "Instalando Lxdm..."
+apt install -y lxdm
+sistemctl --user enable lxdm
+sistemctl --user start lxdm
 
 # Mensaje final
 echo "La instalacion termino"
-echo "Copiar archivos de configuracion"
