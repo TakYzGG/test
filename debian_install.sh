76b9b453936ed412312bafa8d5ac0762ad133fec
TakYzGG <157247602+TakYzGG@users.noreply.github.com>
2024-07-16 18:26:48 -0300

    Add files via upload

--- debian_install.sh
+++ debian_install.sh
@@ -7,7 +7,8 @@ fluxbox=https://github.com/TakYzGG/Dotfiles
 
 # Actualizar el sistema
 echo "Actualizando el sistema..."
-apt install update
+apt update
+apt upgrade
 
 # Instalar servidor grafico y terminal
 echo "Instalando el servidor grafico..."
@@ -26,7 +27,11 @@ apt install -y fluxbox dmenu feh
 
 # Instalar Programas adicionales
 echo "Instalando programas adicionales..."
-apt install -y wget git firefox btop galculator xreader mirage arandr l3afpad gparted xarchiver thunar thunar-volman thunar-archive-plugin pavucontrol vlc audacious libreffice lxappearance
+apt install -y wget git firefox btop galculator mirage arandr l3afpad gparted xarchiver thunar thunar-volman thunar-archive-plugin pavucontrol vlc audacious libreffice lxappearance
+
+# Descargar temas
+echo "Descargando temas..."
+apt install -y papirus-icon-theme arc-theme arch-icon-theme
 
 # Configurar Fluxbox
 echo "Configurando fluxbox..."
@@ -36,6 +41,11 @@ rm -rf /home/$usuario/.fluxbox
 mv Dotfiles/.fluxbox /home/$usuario
 mv Dotfiles/Wallpapers /home/$usuario
 rm -rf Dotfiles
+mkdir -p /home/$usuario/.config/gtk-3.0
+touch /home/$usuario/.config/gtk-3.0/settings.ini
+echo "[Settings]" >> /home/$usuario/.config/gtk-3.0/settings.ini
+echo "gtk-theme-name=Arch-Dark" >> /home/$usuario/.config/gtk-3.0/settings.ini
+echo "gtk-icon-theme-name=Papirus-Dark" >> /home/$usuario/.config/gtk-3.0/settings.ini
 
 # Configurar bash
 echo "Configurando bash..."
@@ -44,6 +54,21 @@ rm -rf /root/.bashrc
 wget $bashrc
 cp .bashrc /home/$usuario
 cp .bashrc /root
+
+# Crear carpetas personales
+echo "Creando carpetas personales..."
+apt install -y xdg-user-dirs
+xdg-user-dirs-update
+
+# Instalar ufw (Firewall)
+echo "Instalando ufw..."
+apt install -y ufw
+echo "Configurando ufw..."
+ufw allow http
+ufw allow https
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
 
 # Instalar pulseaudio y habilitarlo
 echo "Instalando pulseaudio..."
